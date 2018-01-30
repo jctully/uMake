3f418620bc153e5967e25eb73ce6364c257d26ba
Joseph Tully <tullyj2@wwu.edu>
2018-01-30 15:08:05 -0800

    Issue #1 close

---
+++ arg_parse.h
@@ -0,0 +1,9 @@
+#ifndef _arg_parse_
+#define _arg_parse_
+
+/* Called by processline, takes in a line of commands as a string (char*) and
+returns an array of the commands with each position pointing to the first letter of the command. */
+
+char** arg_parse (char* line);
+
+#endif
--- umake.c
+++ umake.c
@@ -10,6 +10,8 @@
 #include <sys/wait.h>
 #include <ctype.h>
 
+#include "arg_parse.h"
+
 /* CONSTANTS */
 
 
@@ -22,10 +24,7 @@
  */
 void processline(char* line);
 
-/* Called by processline, takes in a line of commands as a string (char*) and
-returns an array of the commands with each position pointing to the first letter of the command. */
 
-char** arg_parse (char* line);
 /* Main entry point.
  * argc    A count of command-line arguments
  * argv    The command-line argument valus
@@ -97,54 +96,4 @@ void processline (char* line) {
   }
   }
   free(args);
-}
-
- /* given a line of commands, the function will return the number of arguments
- as an int, skipping whitespace*/
-int countArgs (char* line) {
-  int i = 0;
-  int onWhitespace = 1;
-  int countargs = 0;
-  while (line[i] != '\0') {
-    if (isspace(line[i])) {
-      onWhitespace = 1;
-    }
-    else if (onWhitespace == 1){
-      countargs += 1;
-      onWhitespace = 0;
-    }
-    i++;
-  }
-  return countargs;
-}
-/* Arg Parse the command line to parse
- Called by processline, takes in a line of commands as a string (char*) and
-returns an array of the commands with each position pointing to the first letter of the command.
-To do this it uses a kind of state machine to tell if it is reading whitespace, or moving from
-white space to characters. */
-char** arg_parse (char* line) {
-
-  int count = countArgs(line);
-  char** args = malloc ((count+1) * sizeof(char*));
-
-  int i = 0;
-  int onWhitespace = 1;
-  int countargs = 0;
-  while (line[i] != '\0') {
-    if (isspace(line[i])) {
-      if (onWhitespace == 0) {
-        line[i] = '\0';
-      }
-      onWhitespace = 1;
-    }
-    else if (onWhitespace == 1){
-      args[countargs] = &line[i];
-      countargs += 1;
-      onWhitespace = 0;
-    }
-    i++;
-  }
-  args[count]=NULL;
-  return args;
-
 }
